d15cf2754b89c1eef618c894457ac5d8a9b611b5
Yehiashamel <102812060+Yehiashamel@users.noreply.github.com>
2022-07-14 20:34:34 +0200

    master

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let menuBtn = document.querySelector('.btn');
 let navbar = document.querySelector("#ul");
 let nav = document.querySelector(".nav");
+let icon =document.getElementById("icon")
 menuBtn.onclick = function(){
     navbar.classList.toggle("active");
+    icon.classList.toggle("fa-xmark")
 };
 document.body.onscroll = () =>{
-    navbar.classList.remove("active")
+    navbar.classList.remove("active");
+    icon.classList.remove("fa-xmark");
 };
